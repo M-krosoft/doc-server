e56c3ead6267a8e09303e635808157142f8be1f7
SmallCelestial <105149426+SmallCelestial@users.noreply.github.com>
2024-10-09 22:25:31 +0200

    vision_service review

--- services/vision_service.py
+++ services/vision_service.py
@@ -7,25 +7,27 @@ class VisionService:
         self.api_key = os.getenv('GOOGLE_API_KEY')
         self.text_recognition_url = f"https://vision.googleapis.com/v1/images:annotate?key={self.api_key}"
 
-    def detect_text_in_image(self, image_content):
+    def detect_text_in_image(self, image_content: bytes) -> str | None:
 
         encoded_image = base64.b64encode(image_content).decode('utf-8')
         request_data = self.get_text_recognition_request_body(encoded_image)
-
-        response = requests.post(self.text_recognition_url, json=request_data)
-
-        if response.status_code == 200:
+        
+        try:
+            response = requests.post(self.text_recognition_url, json=request_data)
+            response.raise_for_status()
+            
             json_response = response.json()
             detected_text = json_response['responses'][0].get('textAnnotations', [])
+            
             if detected_text:
                 return detected_text[0]['description']
             else:
                 return None
-        else:
-            raise Exception(f"API Error: {response.status_code}, Message: {response.text}")
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"API error: {e}")
 
     @staticmethod
-    def get_text_recognition_request_body(encoded_image):
+    def get_text_recognition_request_body(encoded_image: str) -> dict:
         return {
             "requests": [
                 {
